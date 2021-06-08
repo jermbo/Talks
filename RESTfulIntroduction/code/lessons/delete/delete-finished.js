a4ab5b68131dca631b04450d6b96cea5c94afcf3
jermbo <jermbo@users.noreply.github.com>
2021-06-08 09:10:37 -0400

    📦 NEW: Adding Delete Lesson Code

---
+++ RESTfulIntroduction/code/lessons/delete/delete-finished.js
@@ -0,0 +1,27 @@
+const BASE_URL = "https://api.sampleapis.com/movies/";
+
+// ---------------------------------
+// DELETE
+const deleteForm = document.querySelector(".delete-form");
+const deleteButton = deleteForm.querySelector("button");
+
+deleteForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const elements = e.target.elements;
+  const movieObject = {
+    id: elements[0].value
+  };
+  deleteMovie(movieObject);
+});
+
+async function deleteMovie(movie) {
+  deleteButton.disabled = true;
+
+  const resp = await fetch(`${BASE_URL}/action-adventure/${movie.id}`, {
+    method: "DELETE"
+  });
+
+  const data = await resp.json();
+  console.table(data);
+  deleteButton.disabled = false;
+}
